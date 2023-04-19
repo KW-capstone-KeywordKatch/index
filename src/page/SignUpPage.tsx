a5f7ead0103a3d16400e7d7192ecf04b8bd2ea1b
LEEGURTS <rmstjd333@gmail.com>
2023-04-19 21:04:48 +0900

    [FE DEBUG] Proxy를 위한 netlify.toml

--- src/page/SignUpPage.tsx
+++ src/page/SignUpPage.tsx
@@ -1,18 +1,7 @@
-import { useUserInfo } from "../State/UserInfo";
 import GlobalNavigationBar from "./../component/GlobalNavigationBar/GlobalNavigationBar";
 import SignUp from "./../component/SignUp/SignUp";
-import HomePage from "./HomePage";
 
 const SignUpPage: React.FunctionComponent = () => {
-  const userInfo = useUserInfo();
-
-  if (userInfo.isLoggined) {
-    return (
-      <>
-        <HomePage />
-      </>
-    );
-  }
   return (
     <>
       <GlobalNavigationBar />
